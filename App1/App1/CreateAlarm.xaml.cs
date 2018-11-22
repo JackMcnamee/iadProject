0a009cf9767e8c8938cbda264f5dd677519253df
JACK MC NAMEE <G00359656@gmit.ie>
2018-11-22 11:23:24 +0000

    Updated timer and maths quiz

--- App1/App1/CreateAlarm.xaml.cs
+++ App1/App1/CreateAlarm.xaml.cs
@@ -1,28 +1,93 @@
-﻿using System;
+﻿using Android.App;
+using Android.OS;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace App1
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CreateAlarm : ContentPage
 	{
-		public CreateAlarm ()
+        DateTime triggerTime;
+
+        public CreateAlarm ()
 		{
 			InitializeComponent ();
-		}
 
-        // Save alarm and redirect to MainPage
-        private void btnSaveAlarm_Clicked(object sender, EventArgs e)
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick()
         {
-            // back to MainPage
-            Navigation.PushModalAsync(new MainPage());
+            if (@switchSaveAlarm.IsToggled && DateTime.Now >= triggerTime)
+            {
+                @switchSaveAlarm.IsToggled = false;
+                DisplayAlert("Alarm name", nameAlarm.Text, "Ok");
+                Navigation.PushModalAsync(new MathsProblems());
 
+                // vibration is turned on
+                /*if (@switchVibration.IsToggled)
+                {
+                    @switchSaveAlarm.IsToggled = false;
+                    DisplayAlert("Alarm name", nameAlarm.Text, "Ok");
+                    object Vibration = null;
+                    try
+                    {
+                        // Vibration.Vibrate();
+
+                    }
+                    catch (Exception)
+                    {
+                        // other error has occurred
+                    }
+
+                }
+
+                // alarm sound turned on
+                else if(switchAlarmSound.IsToggled)
+                {
+                    @switchSaveAlarm.IsToggled = false;
+                    DisplayAlert("Alarm name", nameAlarm.Text, "Ok");
+                    
+                }*/
+
+            }
+            return true;
+        }
+
+        void OnTimePickerPropertyChanged(object obj, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+            }
+        }
+
+        void OnSwitchToggled(object obj, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+        }
+
+        void SetTriggerTime()
+        {
+            if (@switchSaveAlarm.IsToggled)
+            {
+                triggerTime = DateTime.Today + timePicker.Time;
+                Navigation.PushModalAsync(new MainPage());
+
+                if (triggerTime < DateTime.Now)
+                {
+                    triggerTime += TimeSpan.FromDays(1);
+                }
+            }
         }
     }
 }
---
+++ App1/App1/MathsProblems.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MathsProblems : ContentPage
+	{
+        Random randomizer = new Random();
+
+        int addend1;
+        int addend2;
+
+        public MathsProblems ()
+		{
+            InitializeComponent();
+		}
+
+        public void startQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            
+            answer.SetValue = 0;
+
+        }
+
+        private void IsAnswerRight()
+        {
+            if (CheckTheAnswer())
+            {
+                DisplayAlert("Correct answers", "Well done", "Ok");
+                // stopAlarm();
+            }
+            else
+            {
+                DisplayAlert("Incorrect answers", "Unlucky", "Try again");
+            }
+
+        }
+
+        bool CheckTheAnswer()
+        {
+            if (addend1 + addend2 == answer.GetValue)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+               
+        }
+    }
+}
